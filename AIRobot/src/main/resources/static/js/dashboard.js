71fa28a5464ec708ab94b6a1a7ba0bcdafa31694
boyiyun1991 <11123392456@qq.com>
2019-08-10 13:53:59 +0800

    【提交单号】：00001 【提交记录】：4K分辨率适屏 【提交人员】：Rasen

--- AIRobot/src/main/resources/static/js/dashboard.js
+++ AIRobot/src/main/resources/static/js/dashboard.js
@@ -1,4 +1,15 @@
 $(function(){
+	
+	var axisLabelFontSize = 14;
+	let barWidth = "26px";
+	let bgBarWidth = "20px";
+	let windowWidth = $(window).width();
+	if (windowWidth >= 3840) {
+		axisLabelFontSize = 28;
+		barWidth = "50px";
+		bgBarWidth = "40px";
+	}
+	
 	initClock();
 	initSafeChart();
 	initIllegalChart();
@@ -15,52 +26,6 @@ $(function(){
 	});
 	
 	function initClock() {
-		/*var clock = $('#clock'),
-			alarm = clock.find('.alarm'),
-			ampm = clock.find('.ampm');
-		var alarm_counter = -1;
-		var digit_to_name = 'zero one two three four five six seven eight nine'.split(' ');
-		var digits = {};
-		var positions = [
-			'h1', 'h2', ':', 'm1', 'm2', ':', 's1', 's2'
-		];
-		var digit_holder = clock.find('.digits');
-		$.each(positions, function(){
-			if(this == ':'){
-				digit_holder.append('<div class="dots">');
-			} else{
-				var pos = $('<div>');
-				for(var i=1; i<8; i++){
-					pos.append('<span class="d' + i + '">');
-				}
-				digits[this] = pos;
-				digit_holder.append(pos);
-			}
-
-		});
-		var weekday_names = 'MON TUE WED THU FRI SAT SUN'.split(' '),
-			weekday_holder = clock.find('.weekdays');
-		$.each(weekday_names, function(){
-			weekday_holder.append('<span>' + this + '</span>');
-		});
-		var weekdays = clock.find('.weekdays span');
-		(function update_time(){
-			var now = moment().format("HHmmssdA");
-			digits.h1.attr('class', digit_to_name[now[0]]);
-			digits.h2.attr('class', digit_to_name[now[1]]);
-			digits.m1.attr('class', digit_to_name[now[2]]);
-			digits.m2.attr('class', digit_to_name[now[3]]);
-			digits.s1.attr('class', digit_to_name[now[4]]);
-			digits.s2.attr('class', digit_to_name[now[5]]);
-			var dow = now[6];
-			dow--;
-			if(dow < 0){
-				dow = 6;
-			}
-			weekdays.removeClass('active').eq(dow).addClass('active');
-			ampm.text(now[7]+now[8]);
-			setTimeout(update_time, 1000);
-		})();*/
 		var newTime = '';
 	    getLangDate();
 	}
@@ -100,7 +65,7 @@ $(function(){
 					text: "单位：次数",
 					x: "right",
 					textStyle: {
-					    fontSize: 14,
+					    fontSize: axisLabelFontSize,
 					    color: '#ECECEE'
 					}  
 				},
@@ -131,7 +96,8 @@ $(function(){
 							axisLabel: {
 								show: true,
 								textStyle: {
-									 color: "#BDD8E7"
+									 color: "#BDD8E7",
+									 fontSize: axisLabelFontSize
 								}
 							}
 						},
@@ -162,7 +128,8 @@ $(function(){
 								axisLabel: {
 									show: true,
 									textStyle: {
-										 color: "#BDD8E7"
+										 color: "#BDD8E7",
+										 fontSize: axisLabelFontSize
 									}
 								}
 							}
@@ -170,7 +137,7 @@ $(function(){
 						series : [{
 									name:'数据',
 									type:'bar',
-									barWidth: '26px',
+									barWidth: barWidth,
 									xAxisIndex: 0,
 									zlevel: 1,
 									data:[{
@@ -213,7 +180,7 @@ $(function(){
 								{
 									name:'数据',
 									type:'bar',
-									barWidth: '20px',
+									barWidth: bgBarWidth,
 									xAxisIndex: 1,
 									zlevel: 0,
 									data:[{
@@ -292,6 +259,7 @@ $(function(){
 		};
 		var safeChart = echarts.init(document.getElementById('safeChart'));
 		safeChart.setOption(safeOption);
+		safeChart.resize();
 	};
 	
 	function initIllegalChart() {
@@ -302,7 +270,7 @@ $(function(){
 					x: "center",
 					padding: [25,0,20,0],
 					textStyle: {
-					    fontSize: 18,
+						fontSize: axisLabelFontSize,
 					    color: '#0795EB'
 					},
 					subtext: "单位：次数",
@@ -333,7 +301,8 @@ $(function(){
 					axisLabel: {
 						show: true,
 						textStyle: {
-							color: "#BDD8E7"
+							color: "#BDD8E7",
+							fontSize: axisLabelFontSize,
 						}
 					},
 					data: ['6-08', '6-09', '6-10', '6-11', '6-12', '6-13', '6-14']
@@ -368,7 +337,8 @@ $(function(){
 						axisLabel: {
 							show: true,
 							textStyle: {
-								 color: "#BDD8E7"
+								 color: "#BDD8E7",
+								 fontSize: axisLabelFontSize,
 							}
 						}
 					}
@@ -392,9 +362,9 @@ $(function(){
 					}
 				}]
 		};
-
 		var illegalChart = echarts.init(document.getElementById('illegalChart'));
 		illegalChart.setOption(illegalOption);
+		illegalChart.resize();
 	}
 	
 	
@@ -406,7 +376,7 @@ $(function(){
 					x: "center",
 					padding: [25,0,20,0],
 					textStyle: {
-					    fontSize: 18,
+						fontSize: axisLabelFontSize,
 					    color: '#0795EB'
 					},
 					subtext: "单位：次数",
@@ -437,7 +407,8 @@ $(function(){
 					axisLabel: {
 						show: true,
 						textStyle: {
-							color: "#BDD8E7"
+							color: "#BDD8E7",
+							fontSize: axisLabelFontSize,
 						}
 					},
 					data: ['6-08', '6-09', '6-10', '6-11', '6-12', '6-13', '6-14']
@@ -472,7 +443,8 @@ $(function(){
 						axisLabel: {
 							show: true,
 							textStyle: {
-								 color: "#BDD8E7"
+								 color: "#BDD8E7",
+								 fontSize: axisLabelFontSize,
 							}
 						}
 					}
@@ -492,13 +464,12 @@ $(function(){
 					}},
 					itemStyle: {
 						color: "#FCB609"
-
 					}
 				}]
 		};
-
 		var dangerChart = echarts.init(document.getElementById('dangerChart'));
 		dangerChart.setOption(dangerOption);
+		dangerChart.resize();
 	}
 	
 	function initComplexChart01() {
@@ -508,7 +479,7 @@ $(function(){
 					text: "单位：次数",
 					x: "right",
 					textStyle: {
-					    fontSize: 14,
+						fontSize: axisLabelFontSize,
 					    color: '#ECECEE'
 					}  
 				},
@@ -539,7 +510,8 @@ $(function(){
 						axisLabel: {
 							show: true,
 							textStyle: {
-								color: "#BDD8E7"
+								color: "#BDD8E7",
+								fontSize: axisLabelFontSize,
 							}
 						}
 				},
@@ -557,7 +529,8 @@ $(function(){
 							axisLabel: {
 								show: false,
 								textStyle: {
-									 color: "#BDD8E7"
+									 color: "#BDD8E7",
+									 fontSize: axisLabelFontSize,
 								}
 							}
 						}
@@ -565,7 +538,7 @@ $(function(){
 					series : [{
 								name:'数据',
 								type:'bar',
-								barWidth: '26px',
+								barWidth: barWidth,
 								data:[{
 							        value : 150,
 							        itemStyle:{
@@ -592,6 +565,7 @@ $(function(){
 		};
 		var complexChart01 = echarts.init(document.getElementById('complexChart01'));
 		complexChart01.setOption(complexOption01);
+		complexChart01.resize();
 	};
 	
 	function initComplexChart02() {
@@ -605,7 +579,8 @@ $(function(){
 					y : 'bottom',
 					data:['未佩戴安全帽','行人非法闯入','明火危险','人员聚集'],
 					textStyle: {
-						color: "#A9C2D3"
+						color: "#A9C2D3",
+						fontSize: axisLabelFontSize,
 					}
 				},
 				calculable : true,
@@ -613,15 +588,15 @@ $(function(){
 					{
 						name:'半径模式',
 						type:'pie',
-						radius : [30, 80],
+						radius : ['30%', '80%'],
 						center : ['50%', '50%'],
 						roseType : 'radius',
 						label: {
 							normal: {
 								show: true,
-//								textStyle:{
-//									color: "#fff",
-//								}
+								textStyle:{
+									fontSize: axisLabelFontSize,
+								}
 							},
 
 						},
@@ -645,6 +620,7 @@ $(function(){
 		};
 		var complexChart02 = echarts.init(document.getElementById('complexChart02'));
 		complexChart02.setOption(complexOption02);
+		complexChart02.resize();
 
 	}
 	
