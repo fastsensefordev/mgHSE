053a0df9a1b90cd86744e1021f78b67dbb874f69
boyiyun1991 <11123392456@qq.com>
2019-09-15 20:13:08 +0800

    【提交单号】：00001 【提交记录】：数据表结构 【提交人员】：Rasen

---
+++ AIRobot/sys-config/robot.sql
@@ -0,0 +1,148 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50528
+Source Host           : localhost:3306
+Source Database       : robot
+
+Target Server Type    : MYSQL
+Target Server Version : 50528
+File Encoding         : 65001
+
+Date: 2019-09-15 20:12:30
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tbl_address
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_address`;
+CREATE TABLE `tbl_address` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `ipType` int(2) DEFAULT '0' COMMENT '类型（0 算法 1 其他）',
+  `ip` varchar(512) DEFAULT NULL COMMENT 'IP地址',
+  `level` int(2) DEFAULT NULL COMMENT '级别 1 2 3',
+  `area` varchar(255) DEFAULT NULL COMMENT '区域',
+  `cameraId` varchar(512) DEFAULT NULL COMMENT '摄像头id',
+  `location` varchar(512) DEFAULT NULL COMMENT '摄像头位置',
+  `userId` int(12) DEFAULT NULL COMMENT '用户id',
+  `createUser` varchar(255) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  `pid` int(12) DEFAULT NULL,
+  `status` int(2) DEFAULT '0' COMMENT '状态（0未删除,1删除）',
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tbl_admin_key
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_admin_key`;
+CREATE TABLE `tbl_admin_key` (
+  `adminKey` varchar(255) DEFAULT NULL COMMENT '找回超级密码'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tbl_alarm_info
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_alarm_info`;
+CREATE TABLE `tbl_alarm_info` (
+  `id` bigint(12) NOT NULL AUTO_INCREMENT,
+  `sourceId` bigint(20) DEFAULT NULL,
+  `alarmId` int(10) DEFAULT NULL COMMENT 'ID',
+  `alarmName` varchar(2048) DEFAULT NULL COMMENT '报警事件名称,算法名称',
+  `server` varchar(512) DEFAULT NULL,
+  `takePic1` longtext COMMENT '图片1-报警对象图片',
+  `alarmTime` datetime DEFAULT NULL COMMENT '报警时间',
+  `isDelete` int(1) DEFAULT '0' COMMENT '是否处理',
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23545 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tbl_alarm_type
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_alarm_type`;
+CREATE TABLE `tbl_alarm_type` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `alarmNameEn` varchar(1024) DEFAULT NULL COMMENT '算法类型（名称 唯一的）',
+  `alarmName` varchar(1024) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tbl_config
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_config`;
+CREATE TABLE `tbl_config` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `config` varchar(512) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tbl_login_log
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_login_log`;
+CREATE TABLE `tbl_login_log` (
+  `id` bigint(12) NOT NULL AUTO_INCREMENT,
+  `uId` int(10) DEFAULT NULL COMMENT '用户id',
+  `loginTime` datetime DEFAULT NULL COMMENT '登录时间',
+  `ip` varchar(255) DEFAULT NULL COMMENT '访问ip地址',
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tbl_task_errorlog
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_task_errorlog`;
+CREATE TABLE `tbl_task_errorlog` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `info` varchar(2048) DEFAULT NULL,
+  `updateTime` datetime DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tbl_template
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_template`;
+CREATE TABLE `tbl_template` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `templateName` varchar(255) DEFAULT '' COMMENT '模板名称',
+  `href` varchar(512) DEFAULT NULL,
+  `imgUrl` varchar(1024) DEFAULT NULL,
+  `alarmId` varchar(218) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+  `userId` int(12) DEFAULT NULL COMMENT '关联tbl_user表主键id',
+  `createUser` varchar(255) DEFAULT NULL COMMENT '创建者',
+  `pid` int(12) DEFAULT NULL,
+  `status` int(2) DEFAULT '0' COMMENT '状态（0未删除,1删除）',
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=163 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tbl_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_user`;
+CREATE TABLE `tbl_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键UID',
+  `phone` varchar(255) DEFAULT NULL,
+  `userName` varchar(255) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(1024) DEFAULT NULL COMMENT '用户密码(密文）',
+  `userType` int(1) DEFAULT '0' COMMENT '用户类型（0为管理员 1未超级管理员）',
+  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+  `lastLoginTime` datetime DEFAULT NULL COMMENT '最新登录时间',
+  `status` int(2) DEFAULT '0' COMMENT '是否删除（0 未删除 1已删除）',
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '时间戳',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userName` (`userName`) USING BTREE,
+  UNIQUE KEY `phone` (`phone`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
