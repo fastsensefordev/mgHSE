2acdc66dec345d96f1c58ecb2210e0ff1feb187b
boyiyun1991 <11123392456@qq.com>
2019-11-10 11:39:15 +0800

    【提交单号】：00001 【提交记录】：定时任务逻辑修改 【提交人员】：Rasen

--- AIRobot/src/main/java/com/hs/timmer/AlarmTimmer.java
+++ AIRobot/src/main/java/com/hs/timmer/AlarmTimmer.java
@@ -21,7 +21,7 @@ public class AlarmTimmer {
     	alarmService.parseData();
 	}
     
-    //@Scheduled(cron= "0/20 * * * * ? ")
+    @Scheduled(cron= "0/20 * * * * ? ")
 	public void realTimeTask() {
     	alarmService.realTimeTask();
 	}
